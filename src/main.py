76b2fc3ab9cb6a46f337d832a36725383d283e0b
Tim Germann <tim_germann@bluewin.ch>
2023-10-02 14:59:10 +0200

    implemented main

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import cv2
+
+from cameraCapture import Camera
+
+if __name__ == "__main__":
+    camera = Camera(camera_index=0)
+
+    try:
+        while True:
+            frame = camera.capture_frame()
+            cv2.imshow("Camera Capture", frame)
+
+
+
+
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    except KeyboardInterrupt:
+        print("Exiting the program.")
+
+    camera.release()
+    cv2.destroyAllWindows()
