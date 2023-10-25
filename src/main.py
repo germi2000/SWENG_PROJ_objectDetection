93708fccfd8225abff1947801fb8c61eb01547e7
Yannick Kohler <yannick.kohler@hispeed.ch>
2023-10-25 14:51:13 +0200

    changed text color to black

--- src/main.py
+++ src/main.py
@@ -1,18 +1,22 @@
 import cv2
 
 from cameraCapture import Camera
+from objectDetection import ShapeDetector
 
 if __name__ == "__main__":
-    camera = Camera(camera_index=1)
+    camera = Camera(camera_index=0)
 
     try:
         while True:
             frame = camera.capture_frame()
             cv2.imshow("Camera Capture", frame)
 
+          # ShapeDetector-Objekt erstellen und Formen erkennen
+            shape_detector = ShapeDetector()
+            result, detected_shapes = shape_detector.detect(frame)
 
-
-
+            # Livebild anzeigen
+            cv2.imshow("Shape Detection", result)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
